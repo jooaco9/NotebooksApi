10fd2a98260f52a93de5fe37f4595f95192f8d2b
jooaco9 <joaquin.corbo9@gmail.com>
2025-04-21 19:25:16 -0300

    import de app de main

--- api/__init__.py
+++ api/__init__.py
@@ -2,3 +2,5 @@ from api.data.notebooks_data import NotebookData
 from api.utilities.models import Notebook, OSType
 from api.utilities.docs import tags_metadata
 from api.routes.notebooks import router as notebooks_routes
+
+from api.main import app
